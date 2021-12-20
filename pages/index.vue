68383a11c6282aa3978193eba3e68a8d9b2ba4e0
Louis <louis@wespond.de>
2021-12-19 21:48:49 -0800

    Initial styling of home screen.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <Tutorial/>
+  <div id="rootContainer" class="p-10 h-screen relative">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-2">
+        <div class="">
+          <HeuteIsLogo />
+          <h3>Monday, Dec 17th</h3>
+        </div>
+        <div class="text-right">
+          <h3>4:30</h3>
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto bottom-1 absolute">
+      <h1>What will you build today?</h1>
+      <div class="yearProgressWrapper">
+          <span>95%</span>
+      </div>
+      <img src="~/static/img/sloth_icon.png" alt="" width="35px">
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +28,10 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+
+<style scoped>
+#rootContainer {
+  background-color: black;
+  color: white;
+}
+</style>
