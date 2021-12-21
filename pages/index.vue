74697841c41bc9e6b9aca8229f368bd45315c9e8
Louis <louis@wespond.de>
2021-12-20 21:06:52 -0800

    Made dashboard page more beautiful & fixed tailwind hot reload bug.

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="rootContainer" class="p-10 h-screen relative">
-    <div class="container mx-auto">
-      <div class="grid grid-cols-2">
-        <div class="">
-          <HeuteIsLogo />
-          <h3>Monday, Dec 17th</h3>
+  <div id="rootContainer" class="p-10 h-screen bg-cover bg-center text-white" style="background-image: url('https://images.unsplash.com/photo-1515896769750-31548aa180ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80')" >
+    <div class="container mx-auto h-full relative z-10">
+      <div class="container w-full">
+        <div class="grid grid-cols-2">
+          <div class="">
+            <HeuteIsLogo class="mb-1" />
+            <h3>Monday, Dec 17th</h3>
+          </div>
+          <div class="text-right">
+            <h3 class="text-2xl">4:30</h3>
+          </div>
         </div>
-        <div class="text-right">
-          <h3>4:30</h3>
+      </div>
+      <div class="max-w-s w-full absolute top-2/4 left-2/4 transform -translate-y-2/3 -translate-x-1/2 text-center">
+        <h1 class="text-6xl">What will you build today?</h1>
+      </div>
+      <div class="container w-full bottom-1 absolute">
+        <div class="grid grid-cols-2">
+          <div class="">
+            <img src="~/static/img/sloth_icon.png" alt="" width="35px">
+          </div>
+          <div class="text-right">
+            <div class="yearProgressWrapper">
+                <span>95%</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="container mx-auto bottom-1 absolute">
-      <h1>What will you build today?</h1>
-      <div class="yearProgressWrapper">
-          <span>95%</span>
-      </div>
-      <img src="~/static/img/sloth_icon.png" alt="" width="35px">
-    </div>
+    <div class="w-full h-1/2 absolute top-0 left-0 z-0" style="background: linear-gradient(180deg, #000000c7, transparent);" />
+    <div class="w-full h-1/2 absolute bottom-0 left-0 z-0" style="background: linear-gradient(0deg, #000000c7, transparent);" />
   </div>
 </template>
 
@@ -30,8 +42,4 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-#rootContainer {
-  background-color: black;
-  color: white;
-}
 </style>
