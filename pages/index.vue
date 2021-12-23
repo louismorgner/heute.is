05ab9c50c824d614ae8fb9dc8d2d4eba545d078f
Louis <louis@wespond.de>
2021-12-23 12:44:46 -0800

    Added interactive prompt for goal setting.

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,18 @@
         </div>
       </div>
       <div class="max-w-s w-full absolute top-2/4 left-2/4 transform -translate-y-2/3 -translate-x-1/2 text-center">
-        <h1 class="text-6xl">What will you build today?</h1>
+        <h1 class="text-6xl">{{ headline }}</h1>
+        <div v-if="!showBuildPrompt">
+          <button class="btn btn-outline btn-circle btn-xs mt-5" style="color: rgb(255 255 255 / 31%)" @click="toggleBuildPrompt(true)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current">   
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>                       
+            </svg>
+          </button>
+        </div>
+        <textarea v-if="showBuildPrompt" v-model="buildPrompt" rows="1" placeholder="Today, I will..." class="buildPromptForm mt-8" @keypress="detectBuildPromptChange($event)" @keyup.enter="toggleBuildPrompt(false)" />
+        <div class="block text-center pt-5">
+          <div v-show="showBuildPromptSubmissionHint" class="badge">Hit 'Enter' to save your daily goal.</div>
+        </div>
       </div>
       <div class="container w-full bottom-1 absolute">
         <div class="grid grid-cols-2">
@@ -43,7 +54,11 @@ export default Vue.extend({
   name: 'DashboardPage',
   data() {
     return {
-      time: ""
+      time: "",
+      buildPrompt: "",
+      headline: "What will you build today?",
+      showBuildPrompt: true,
+      showBuildPromptSubmissionHint: false
     }
   },
   computed: {
@@ -88,6 +103,25 @@ export default Vue.extend({
       } else {
         this.time = now.getHours() + ":" + now.getMinutes()
       }
+    },
+    detectBuildPromptChange() {
+      if(this.buildPrompt.length > 3) {
+        this.showBuildPromptSubmissionHint = true;
+      } else {
+        this.showBuildPromptSubmissionHint = false;
+      }
+    },
+    toggleBuildPrompt(isShowing) {
+      // Handles the submission of a build prompt
+      if(isShowing) {
+        this.showBuildPromptSubmissionHint = true;
+        this.showBuildPrompt = true;
+        this.headline = "What will you build today?";
+      } else {
+        this.showBuildPromptSubmissionHint = false;
+        this.showBuildPrompt = false;
+        this.headline = this.buildPrompt;
+      }
     }
   },
 })
@@ -97,5 +131,18 @@ export default Vue.extend({
 progress.progress.progress-primary {
     height: 3px;
     background-color: #ffffff30;
+}
+
+textarea.buildPromptForm {
+    background: transparent;
+    border-bottom: 3px white solid;
+    width: 40%;
+    font-size: 26px;
+    text-align: center;
+}
+
+textarea:focus{
+    outline: none!important;
+    box-shadow: none!important;
 }
 </style>
