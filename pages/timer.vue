6290a34f18fa652e1315c2a711759698b4c4b356
Louis <louis@wespond.de>
2021-12-25 12:53:43 -0800

    Added first version of rating element to timer.

--- pages/timer.vue
+++ pages/timer.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="timerWrapper" class="bg-black h-screen w-full">
     <div class="max-w-4xl w-full absolute top-2/4 left-2/4 transform -translate-y-2/3 -translate-x-1/2 text-center">
-      <h1 class="timer">{{ timerString }}</h1>
-      <p>{{ focusText }}</p>
+      <div v-if="!isFinished">
+        <h1 class="timer">{{ timerString }}</h1>
+        <p>{{ focusText }}</p>
+      </div>
+      <div v-else>        
+        <input type="checkbox" id="st1" value="1">
+        <label for="st1"></label> 
+        <input type="checkbox" id="st2" value="2" />
+        <label for="st2"></label>
+        <input type="checkbox" id="st3" value="3" />
+        <label for="st3"></label>
+        <input type="checkbox" id="st4" value="4" />
+        <label for="st4"></label>
+        <input type="checkbox" id="st5" value="5" />
+        <label for="st5"></label>
+      </div>
     </div>
   </div>
 </template>
@@ -70,8 +84,48 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .timer {
   font-size: 10rem;
+}
+input {
+  border: 0;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  position: absolute !important;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  opacity: 0;
+}
+
+label {
+  position: relative;
+  float: right;
+  color: #C8C8C8;
+}
+
+label:before {
+  margin: 5px;
+  content: "\f005";
+  font-family: FontAwesome;
+  display: inline-block;
+  font-size: 1.5em;
+  color: #ccc;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+input:checked ~ label:before {
+  color: #FFC107;
+}
+
+label:hover ~ label:before {
+  color: #ffdb70;
+}
+
+label:hover:before {
+  color: #FFC107;
 }
 </style>
