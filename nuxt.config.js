08b785ba50f421a248e6e0845306706c08cf2a3f
Louis <louis@wespond.de>
2021-12-27 17:47:48 -0800

    Added favicons.

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,14 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
