e14259fc8108505813474a13c87d3bfce086a5e2
Louis <louis@wespond.de>
2022-01-23 15:24:51 +0100

    small fix.

--- components/InboxModal.vue
+++ components/InboxModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="showModal" class="fixed w-full h-full z-50 flex text-center justify-center items-center" style="background: #00000082">
         <div class="max-w-lg rounded-lg p-14" style="background:black">
-            <h3 class="text-2xl mb-7">What would you like to add to your inbox?</h3>
+            <h3 class="text-2xl mb-7 text-white">What would you like to add to your inbox?</h3>
             <input ref="inputInbox" v-model="inboxText" type="text" placeholder="Inbox item..." class="input input-bordered w-full" @keyup.enter="addInboxItem">
         </div>
     </div>
